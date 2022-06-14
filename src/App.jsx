d6a5a1a1ef0bf03184692141a48ab66633b0c9e2
Atabek <kurbanof.atabek@gmail.com>
2022-06-14 17:02:19 +0600

    [FT-3] created navbar and imported it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components";
 import { Home, Login, Signup } from "./pages";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
